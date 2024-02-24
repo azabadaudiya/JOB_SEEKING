44b2e12523ab7aae8f0578e74cd7b2c0c1318e27
azabadaudiya <146463403+azabadaudiya@users.noreply.github.com>
2024-02-24 14:03:48 +0530

    Add files via upload

---
+++ frontend/src/components/Home/HeroSection.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { FaBuilding, FaSuitcase, FaUsers, FaUserPlus } from "react-icons/fa";
+
+const HeroSection = () => {
+  const details = [
+    {
+      id: 1,
+      title: "1,25,441",
+      subTitle: "Live Job",
+      icon: <FaSuitcase />,
+    },
+    {
+      id: 2,
+      title: "101220",
+      subTitle: "Companies",
+      icon: <FaBuilding />,
+    },
+    {
+      id: 3,
+      title: "23,34,200",
+      subTitle: "Job Seekers",
+      icon: <FaUsers />,
+    },
+    {
+      id: 4,
+      title: "1,03,761",
+      subTitle: "Employers",
+      icon: <FaUserPlus />,
+    },
+  ];
+  return (
+    <>
+      <div className="heroSection">
+        <div className="container">
+          <div className="title">
+            <h1>Find a job that suits</h1>
+            <h1>your interests and skills</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              voluptate repellat modi quidem aliquid eaque ducimus ipsa et,
+              facere mollitia!
+            </p>
+          </div>
+          <div className="image">
+            <img src="/heroS.jpg" alt="hero" />
+          </div>
+        </div>
+        <div className="details">
+          {details.map((e) => {
+            return (
+              <div className="card" key={e.id}>
+                <div className="icon">{e.icon}</div>
+                <div className="content">
+                  <p>{e.title}</p>
+                  <p>{e.subTitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeroSection;
