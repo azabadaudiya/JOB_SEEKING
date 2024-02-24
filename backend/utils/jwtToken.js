7f13629adf773455362344e03656a6584619539c
azabadaudiya <146463403+azabadaudiya@users.noreply.github.com>
2024-02-24 17:13:07 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,29 +1,16 @@
-import ms from 'ms'; // Import the 'ms' library to parse timespan strings
-
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
-  const cookieExpireDays = process.env.COOKIE_EXPIRE;
-  const jwtExpire = process.env.JWT_EXPIRE;
-
-  // Calculate cookie expiration time
-  const cookieExpireMilliseconds = cookieExpireDays * 24 * 60 * 60 * 1000;
-  const cookieOptions = {
-    expires: new Date(Date.now() + cookieExpireMilliseconds),
-    httpOnly: true,
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true, // Set httpOnly to true
   };
 
-  // Calculate JWT expiration time
-  const jwtExpireMilliseconds = ms(jwtExpire); // Parse JWT_EXPIRE using ms library
-  if (!jwtExpireMilliseconds) {
-    throw new Error('Invalid JWT_EXPIRE value');
-  }
-
-  res.status(statusCode)
-     .cookie('token', token, { ...cookieOptions, maxAge: cookieExpireMilliseconds }) // Use maxAge instead of expires
-     .json({
-        success: true,
-        user,
-        message,
-        token,
-     });
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    message,
+    token,
+  });
 };
